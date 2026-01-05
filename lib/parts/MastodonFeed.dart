8691b1df4756eeca178297f1df4e9e8aa1886d11
Ralf Kraemer <100492510+Ralf-Kraemer@users.noreply.github.com>
2026-01-05 19:40:18 +0100

    Update MastodonFeed.dart

--- lib/parts/MastodonFeed.dart
+++ lib/parts/MastodonFeed.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:toot_ui/toot_ui.dart';
-import 'package:toot_ui/status_form.dart';
 import 'package:toot_ui/models/api/v1/mastodonuser.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
